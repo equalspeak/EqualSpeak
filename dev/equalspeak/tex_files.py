fb2c23a2f34dcea2b512145cde35faecb37e8738
joshmaglione <joshmaglione@gmail.com>
2024-02-12 12:21:19 +0000

    Towards a speaking tree
    - Simplified code (still a bug with environments)
    - Working towards building 'speaking trees'.
        - These are simplified parsing trees.

--- dev/equalspeak/tex_files.py
+++ dev/equalspeak/tex_files.py
@@ -53,74 +53,84 @@ def get_packages(lines:list[str]) -> tuple[str]:
 		packages += s.split(',')
 	return tuple(sorted(packages))
 
+def get_top(re_it, i) -> int:
+	try:
+		return next(re_it).span()[i]
+	except StopIteration:
+		# Getting here means we couldn't find a 'closing' statement.
+		# Therefore the document provided probably does not compile.
+		raise ValueError("File provided does not compile.")
 
-def search(S:str, JUMP=100000, i=0) -> list[tuple]:
-	collect = []
-	all_pats = []
-	for r in ENVS.keys():
-		pattern = re.compile(r)
-		all_pats += [
-			(p.span()[0], r) for p in pattern.finditer(S, endpos=JUMP)
-		]
-	if len(all_pats) == 0: 
-		return collect
+def search(S:str, JUMP=100, i=0) -> list[tuple]:
+	# A function that takes a string r, compiles it with regex, and returns the
+	# iterable from 'finditer'.
+	str_to_iter = lambda r, a, b: re.compile(r).finditer(S, pos=a, endpos=b)
+	all_pats = [
+		(p.span()[0], r) for r in ENVS for p in str_to_iter(r, 0, JUMP)
+	]
+	if len(all_pats) == 0:
+		# If we found no relevant patterns, push forward or stop entirely.
+		if len(S) > JUMP:
+			return search(S[JUMP:], i=i + JUMP)
+		return []
 	all_pats = sorted(all_pats)
 	j, r = all_pats[0]
-	end_pat = re.compile(ENVS[r][0])
-	it = end_pat.finditer(S, pos=j + 1)
-	try:
-		k = next(it).span()[0]
-	except StopIteration:
-		print("Document provided does not compile!")
-		k = len(S) - 1
-	collect.append(
-		((i + j + ENVS[r][2], i + k), r, my_clean(S[j + ENVS[r][2]:k]))
-	)
-	shift = i + k + ENVS[r][2]
-	return collect + search(S[shift:], i=shift)
-
+	j += ENVS[r][2]
+	# At this stage, we know the first relevant pattern occurs at index j. 
+	it = str_to_iter(ENVS[r][0], j, len(S))
+	k = get_top(it, 0)
+	# Now we know the first relevant pattern occurs between j and k.
+	env = ENVS[r][1]
+	if env == 'envi':
+		# If the relevant pattern brings us into a specialized environment, then
+		# we do additional things.
+		env = S[j:k]
+		end_env = fr"\\end{{{env}}}"
+		end_env_pat = re.compile(end_env)
+		it_env = end_env_pat.finditer(S, pos=k)
+		j = k + 1
+		k = get_top(it_env, 0)
+		shift = k + len(end_env)
+	else:
+		shift = k + ENVS[r][2]
+	vertex = ((i + j, i + k), env, my_clean(S[j:k]))
+	return [vertex] + search(S[shift:], i=i + shift)
 
 # Main class
-class latexparse:
+class latex_parse:
 
 	def __init__(self, lines=[], file=None) -> None:
 		self.lines = lines
 		self.file = file
-		# Find 'begin document'
-		i = 0
-		while i < len(lines) and not r'\begin{document}' in lines[i]:
-			i += 1
-		j = i + 1
-		while j < len(lines) and not r'\end{document}' in lines[j]:
-			j += 1
-		self.doclines = (i, j)
-		self.packages = get_packages(lines[:i])
-		self._key_strings = None
+		docline = lambda l: r'\begin{document}' in l or r'\end{document}' in l
+		self.doclines = tuple(
+			map(lambda s: lines.index(s), filter(docline, lines))
+		)
+		self.packages = get_packages(lines[:self.doclines[0]])
+		self._forest = None
 		
 	def __repr__(self) -> str:
-		head = f"A LaTeX parser for\n{self.file}" 
-		packages = ""
-		for p in self.packages:
-			packages += f"\n\t{p} : Not supported"
-		return head + packages
+		return f"A LaTeX parser for\n{self.file}" + ''.join(
+			map(lambda p: f"\n\t{p} : Not supported", self.packages)
+		)
 
-	def key_strings(self) -> list[tuple]:
+	def get_forest(self) -> list[tuple]:
 		"""
-		Returns a list of stuff.
+		Returns a forest of parsing trees.
 		"""
-		if self._key_strings:
-			return self._key_strings
+		if self._forest:
+			return self._forest
 		S = ''.join(self.lines[self.doclines[0] + 1:self.doclines[1]])
-		self._key_strings = search(S)
-		return self._key_strings
+		self._forest = search(S)
+		return self._forest
 	
-	def document(self):
-		return ''.join(self.lines[self.doclines[0] + 1:self.doclines[1]])
+	def document(self, start=0, end=None):
+		text = ''.join(self.lines[self.doclines[0] + 1:self.doclines[1]])
+		return text[start:end]
 	
 	def preamble(self):
 		return ''.join(self.lines[:self.doclines[0]])
 
 def read_tex(file:str):
 	with open(file, 'r') as f:
-		return latexparse(lines=f.readlines(), file=file)
-
+		return latex_parse(lines=f.readlines(), file=file)
--- dev/equalspeak/trees.py
+++ dev/equalspeak/trees.py
@@ -1,1 +1,22 @@
-from treelib import Node, Tree
+# class parse_vertex:
+
+#     def __init__(self, data) -> None:
+#         self.data = data 
+#         self.name = data.__hash__()
+#         self.children = None
+
+#     def __repr__(self) -> str:
+#         return str(self.data)
+    
+#     def __eq__(self, __value: object) -> bool:
+#         pass
+
+class parse_tree:
+
+    def __init__(self, root) -> None:
+        self.root = root
+        self.vertices = {root}
+        self.edges = {}
+
+    def __repr__(self) -> str:
+        pass
