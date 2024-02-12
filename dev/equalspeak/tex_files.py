fc2afbcd4965b7fa60124cb5740c6f28b35c4093
joshmaglione <joshmaglione@gmail.com>
2024-02-12 16:44:42 +0000

    growing trees

--- dev/equalspeak/tex_files.py
+++ dev/equalspeak/tex_files.py
@@ -1,34 +1,4 @@
-import re
 from functools import reduce, partial
-
-ENVS = {
-	r'\$[^$]': (r'\$[^$]', 'math', 1),
-	r'\$\$': (r'\$\$', 'math', 2),
-	r'\\\(': (r'\\\)', 'math', 2),
-	r'\\\[': (r'\\\]', 'math', 2),
-	r'\\begin{': (r'}', 'envi', 7)
-}
-
-# Remove initial and ending whitespaces and replace tabs, new lines, and double
-# whitespace with one whitespace.
-def my_clean(s:str):
-	subs = [			# All different forms of whitespace.
-		('\t', ' '),
-		('\n', ' '),
-		('  ', ' '),
-		('\v', ' '),
-		('\f', ' '),
-		('\r', ' '),
-		('\\ ', ' ')
-	]
-	for x, y in subs:
-		s = s.replace(x, y)
-	# Remove initial and ending whitespaces.
-	pat = re.compile(r'[^\s]')
-	x = list(pat.finditer(s))[0]
-	y = list(pat.finditer(s[::-1]))[0]
-	return s[x.span()[0]:len(s) - y.span()[0]]
-
 
 # Given strings key and s, find all occurrences of key in s and extract the
 # contents of the braces in each instance.
@@ -53,49 +23,6 @@ def get_packages(lines:list[str]) -> tuple[str]:
 		packages += s.split(',')
 	return tuple(sorted(packages))
 
-def get_top(re_it, i) -> int:
-	try:
-		return next(re_it).span()[i]
-	except StopIteration:
-		# Getting here means we couldn't find a 'closing' statement.
-		# Therefore the document provided probably does not compile.
-		raise ValueError("File provided does not compile.")
-
-def search(S:str, JUMP=100, i=0) -> list[tuple]:
-	# A function that takes a string r, compiles it with regex, and returns the
-	# iterable from 'finditer'.
-	str_to_iter = lambda r, a, b: re.compile(r).finditer(S, pos=a, endpos=b)
-	all_pats = [
-		(p.span()[0], r) for r in ENVS for p in str_to_iter(r, 0, JUMP)
-	]
-	if len(all_pats) == 0:
-		# If we found no relevant patterns, push forward or stop entirely.
-		if len(S) > JUMP:
-			return search(S[JUMP:], i=i + JUMP)
-		return []
-	all_pats = sorted(all_pats)
-	j, r = all_pats[0]
-	j += ENVS[r][2]
-	# At this stage, we know the first relevant pattern occurs at index j. 
-	it = str_to_iter(ENVS[r][0], j, len(S))
-	k = get_top(it, 0)
-	# Now we know the first relevant pattern occurs between j and k.
-	env = ENVS[r][1]
-	if env == 'envi':
-		# If the relevant pattern brings us into a specialized environment, then
-		# we do additional things.
-		env = S[j:k]
-		end_env = fr"\\end{{{env}}}"
-		end_env_pat = re.compile(end_env)
-		it_env = end_env_pat.finditer(S, pos=k)
-		j = k + 1
-		k = get_top(it_env, 0)
-		shift = k + len(end_env)
-	else:
-		shift = k + ENVS[r][2]
-	vertex = ((i + j, i + k), env, my_clean(S[j:k]))
-	return [vertex] + search(S[shift:], i=i + shift)
-
 # Main class
 class latex_parse:
 
@@ -107,22 +34,24 @@ class latex_parse:
 			map(lambda s: lines.index(s), filter(docline, lines))
 		)
 		self.packages = get_packages(lines[:self.doclines[0]])
-		self._forest = None
+		self._tree = None
 		
 	def __repr__(self) -> str:
 		return f"A LaTeX parser for\n{self.file}" + ''.join(
 			map(lambda p: f"\n\t{p} : Not supported", self.packages)
 		)
 
-	def get_forest(self) -> list[tuple]:
+	def get_tree(self) -> list[tuple]:
 		"""
-		Returns a forest of parsing trees.
+		Returns a speak-tree for the document.
 		"""
-		if self._forest:
-			return self._forest
+		from .trees import speak_tree, grow_tree
+		if self._tree:
+			return self._tree
 		S = ''.join(self.lines[self.doclines[0] + 1:self.doclines[1]])
-		self._forest = search(S)
-		return self._forest
+		T = speak_tree(S, 'document', (self.doclines[0] + 1, self.doclines[1]))
+		self._tree = grow_tree(T)
+		return self._tree
 	
 	def document(self, start=0, end=None):
 		text = ''.join(self.lines[self.doclines[0] + 1:self.doclines[1]])
--- dev/equalspeak/trees.py
+++ dev/equalspeak/trees.py
@@ -1,22 +1,136 @@
-# class parse_vertex:
+import re
 
-#     def __init__(self, data) -> None:
-#         self.data = data 
-#         self.name = data.__hash__()
-#         self.children = None
+class speak_vertex:
 
-#     def __repr__(self) -> str:
-#         return str(self.data)
-    
-#     def __eq__(self, __value: object) -> bool:
-#         pass
+	def __init__(self, data, name, loc, parent_tree) -> None:
+		self.data = data
+		self.name = name
+		self.location = loc
+		self.parent = parent_tree
+		self.children = []
 
-class parse_tree:
+	def __repr__(self) -> str:
+		return f"{self.name} {self.location}"
 
-    def __init__(self, root) -> None:
-        self.root = root
-        self.vertices = {root}
-        self.edges = {}
+class speak_tree:
 
-    def __repr__(self) -> str:
-        pass
+	def __init__(self, root, name, loc) -> None:
+		self.root = speak_vertex(root, name, loc, self)
+		self.vertices = {self.root}
+
+	def __repr__(self) -> str:
+		head = f"Speak tree:\n"
+		verts = self._dfs()
+		return head + ''.join(["\t"*(t[1]) + f"{t[0]}\n" for t in verts])
+	
+	def _dfs(self) -> list[speak_vertex]:
+		tov = []
+		current = [(self.root, 0)]
+		while current:
+			v, r = current.pop()
+			tov.append((v, r))
+			current += list(map(lambda c: (c, r + 1), v.children))
+		return tov
+
+	def add_children(self, v:speak_vertex, L:list[tuple]):
+		kids = list(map(lambda t: speak_vertex(t[0], t[1], t[2], v.parent), L))
+		v.children = kids[::-1]
+		self.vertices = self.vertices.union(set(kids))
+		return self
+
+	def get_vertex(self, s:str):
+		L = list(filter(lambda v: v.data == s, self.vertices))
+		if L:
+			return L[0]
+		return None
+		
+
+ENVS = {
+	r'\$[^$]': (r'\$[^$]', 'inline math', 1),
+	r'\$\$': (r'\$\$', 'displayed math', 2),
+	r'\\\(': (r'\\\)', 'inline math', 2),
+	r'\\\[': (r'\\\]', 'displayed math', 2),
+	r'\\begin{': (r'}', 'envi', 7), 
+	r'^{': (r'}', 'caret', 2), 
+	r'_{': (r'}', 'underscore', 2)
+}
+
+# Remove initial and ending whitespaces and replace tabs, new lines, and double
+# whitespace with one whitespace.
+def my_clean(s:str):
+	subs = [			# All different forms of whitespace.
+		('\t', ' '),
+		('\n', ' '),
+		('  ', ' '),
+		('\v', ' '),
+		('\f', ' '),
+		('\r', ' '),
+		('\\ ', ' ')
+	]
+	for x, y in subs:
+		s = s.replace(x, y)
+	# Remove initial and ending whitespaces.
+	pat = re.compile(r'[^\s]')
+	x = list(pat.finditer(s))[0]
+	y = list(pat.finditer(s[::-1]))[0]
+	return s[x.span()[0]:len(s) - y.span()[0]]
+
+def search(S:str, JUMP=1000, i=0) -> list[tuple]:
+	# A function that takes a string r, compiles it with regex, and returns the
+	# iterable from 'finditer'.
+	str_to_iter = lambda r, a, b: re.compile(r).finditer(S, pos=a, endpos=b)
+	all_pats = [
+		(p.span()[0], r) for r in ENVS for p in str_to_iter(r, 0, JUMP)
+	]
+	if len(all_pats) == 0:
+		# If we found no relevant patterns, push forward or stop entirely.
+		if len(S) > JUMP:
+			return search(S[JUMP:], i=i + JUMP)
+		return []
+	all_pats = sorted(all_pats)
+	j, r = all_pats[0]
+	j += ENVS[r][2]
+	# At this stage, we know the first relevant pattern occurs at index j. 
+	it = str_to_iter(ENVS[r][0], j, len(S))
+	try:
+		k = next(it).span()[0]
+	except StopIteration:
+		# Getting here means we couldn't find a 'closing' statement.
+		# Therefore the document provided probably does not compile.
+		raise ValueError("File provided does not compile.")
+	# Now we know the first relevant pattern occurs between j and k.
+	env = ENVS[r][1]
+	if env == 'envi':
+		# If the relevant pattern brings us into a specialized environment, then
+		# we do additional things.
+		env = S[j:k]
+		end_env = fr"\end{{{env}}}"
+		j = k + 1
+		try:
+			k = S.index(end_env)
+		except ValueError:
+			# Getting here means we couldn't find a 'closing' statement.
+			# Therefore the document provided probably does not compile.
+			raise ValueError("File provided does not compile.")
+		shift = k + len(end_env)
+	else:
+		shift = k + ENVS[r][2]
+	vertex = (S[j:k], env, (i + j, i + k))
+	return [vertex] + search(S[shift:], i=i + shift)
+
+def grow_tree(T:speak_tree, leaves=None) -> speak_tree:
+	if leaves:
+		v = leaves.pop()
+	else:
+		v = T.root
+		leaves = []
+	print(v)
+	kids = search(v.data)
+	print(kids)
+	if kids:
+		T = T.add_children(v, kids)
+		leaves += list(map(lambda k: T.get_vertex(k[0]), kids))
+	if leaves:
+		return grow_tree(T, leaves=leaves)
+	return T
+	
